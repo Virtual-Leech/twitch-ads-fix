a140da41e3091546b8029bf7f35de7b60f1eec54
Virtual-Leech <48499186+Virtual-Leech@users.noreply.github.com>
2020-11-21 19:51:48 -0800

    Update twitch-videoad.js

--- twitch-videoad.js
+++ twitch-videoad.js
@@ -1,16 +1,18 @@
-/*
 // twitch-videoad.js
 const origFetch = window.fetch;
 window.fetch = (url, init, ...args) => {
 	if (typeof url === "string") {
 		if (url.includes("/access_token")) {
+			console.log("helloworld");
 			 url = url.replace("player_type=site", "player_type=site");
 		} else if (
+			console.log("helloworld");
 			url.includes("/gql") &&
 			init &&
 			typeof init.body === "string" &&
 			init.body.includes("PlaybackAccessToken")
 		) {
+			console.log("helloworld2");
 			 const newBody = JSON.parse(init.body);
 			 newBody.variables.playerType = "site";
 			 init.body = JSON.stringify(newBody);
@@ -18,8 +20,3 @@ window.fetch = (url, init, ...args) => {
 	}
 	return origFetch(url, init, ...args);
 };
-*/
-
-(function() {
-	console.log("helloworld");
-})();
